f1628e2e668bce3ffdc96fdf9b9145433df5140e
YangZhang1990 <yangzhangamber1990@gmail.com>
2016-08-12 13:29:10 -0700

    Add database model
    Create news controller, category controller and source controller.
    news list view and news details new.

--- KapyApp/App_Start/BundleConfig.cs
+++ KapyApp/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace KapyApp
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap_journal.css",
                       "~/Content/site.css"));
         }
     }
